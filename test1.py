e38822ac89f95edc5cde42c8e2e7a85ecde311d8
arara10 <36264912+arara10@users.noreply.github.com>
2018-08-22 01:36:54 +0530

    Deleting test3() in test1.py

--- test1.py
+++ test1.py
@@ -3,5 +3,5 @@ class A:
         print "Inside test 1 func"
     def test2(self):
         print "Inside test 2 func"
-    def test3(self):
-        print "Inside test 3 func"
+    def test$(self):
+        print "I have deleted test 3"
